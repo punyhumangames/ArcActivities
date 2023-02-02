b282824358012b51ed3bcddfc96d3684dc0eb1be
gfleenor <>
2023-02-02 14:45:50 -0800

    Fixed ArcActivitiesPlugin module not loading in shipping

--- Source/ArcActivitiesPlugin/Private/ArcActivitiesPlugin.cpp
+++ Source/ArcActivitiesPlugin/Private/ArcActivitiesPlugin.cpp
@@ -6,51 +6,12 @@
 
 
 
-class FActivitiesPlugin : public IModuleInterface
+class FArcActivitiesPlugin : public IModuleInterface
 {
 	/** IModuleInterface implementation */
-	virtual void StartupModule() override;
-	virtual void ShutdownModule() override;
-
-public:
-
-	/**
-	 * Singleton-like access to this module's interface.  This is just for convenience!
-	 * Beware of calling this during the shutdown phase, though.  Your module might have been unloaded already.
-	 *
-	 * @return Returns singleton instance, loading the module on demand if needed
-	 */
-	static inline FActivitiesPlugin& Get()
-	{
-		return FModuleManager::LoadModuleChecked< FActivitiesPlugin >("ArcActivitiesPlugin");
-	}
-
-	/**
-	 * Checks to see if this module is loaded and ready.  It is only valid to call Get() if IsAvailable() returns true.
-	 *
-	 * @return True if the module is loaded and ready to use
-	 */
-	static inline bool IsAvailable()
-	{
-		return FModuleManager::Get().IsModuleLoaded("ArcActivitiesPlugin");
-	}
+	virtual void StartupModule() override { }
+	virtual void ShutdownModule() override { }
 };
 
-IMPLEMENT_MODULE( FActivitiesPlugin, ActivitiesPlugin )
-
-
-
-void FActivitiesPlugin::StartupModule()
-{
-	// This code will execute after your module is loaded into memory (but after global variables are initialized, of course.)
-}
-
-
-void FActivitiesPlugin::ShutdownModule()
-{
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
-}
-
-
+IMPLEMENT_MODULE(FArcActivitiesPlugin, ArcActivitiesPlugin )
 
